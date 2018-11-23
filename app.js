7f539381b69f9e6bda5c22f24dc00c2a3a6dca3e
Nishant Agarwal <agarwal.nishant07@gmail.com>
2018-11-24 03:20:27 +0530

    Update app.js

--- app.js
+++ app.js
@@ -14,164 +14,162 @@ app.use(bodyParser.json());
 
 // This responds with "Hello" on the homepage
 app.get('/', function (req, res) {
-   console.log("Got a GET request for the homepage");
-   res.end('Hello from Home Page');
+    console.log("Got a GET request for the homepage");
+    res.end('Hello from Home Page');
 });
 
-
-var server = app.listen(process.env.PORT || 3000, function() {
-console.log('API server listening on port: 3000 or ', process.env.PORT)
+var server = app.listen(process.env.PORT || 3000, function () {
+    console.log('API server listening on port: 3000 or ', process.env.PORT)
 })
 
-app.post('/hrbotServer', function (req, res){
-   console.log(req.body.queryResult);
-   let intent = req.body.queryResult.intent['displayName'];
-   let contexts = req.body.queryResult.outputContexts ? req.body.queryResult.outputContexts : [];
-   console.log("contexts");
-   console.log(contexts);
-   let msg= "default msg", contextsObject = {};
-   contexts.map(context => {
-    let contextName = context.name,
-      name = contextName.split("/")[contextName.split("/").length - 1]
-    return contextsObject[name] = context;
-  })
-   console.log(intent);
-   let contextOut = contexts;
-   if (intent == "askPhoneNum"){
-      let phoneNumber = contextsObject["phone_number"];
-      msg = "ab apna sawaal puch";
-      contextOut.push({
-              "name": "projects/${PROJECT_ID}/agent/sessions/${SESSION_ID}/contexts/phone_number",
-              "lifespanCount": 10,
-              "parameters": {
+app.post('/hrbotServer', function (req, res) {
+    console.log(req.body.queryResult);
+    let intent = req.body.queryResult.intent['displayName'];
+    let contexts = req.body.queryResult.outputContexts ? req.body.queryResult.outputContexts : [];
+    console.log("contexts");
+    console.log(contexts);
+    let msg = "default msg",
+        contextsObject = {};
+    contexts.map(context => {
+        let contextName = context.name,
+            name = contextName.split("/")[contextName.split("/").length - 1]
+        return contextsObject[name] = context;
+    })
+    console.log(intent);
+    let contextOut = contexts;
+    if (intent == "askPhoneNum") {
+        let phoneNumber = contextsObject["phone_number"];
+        msg = "ab apna sawaal puch";
+        contextOut.push({
+            "name": "projects/${PROJECT_ID}/agent/sessions/${SESSION_ID}/contexts/phone_number",
+            "lifespanCount": 10,
+            "parameters": {
                 "phone_number": phoneNumber,
                 "phone_number.original": phoneNumber
-              }
-            });
-	   res.json(setResponse(res,msg,contextOut)); 
-   }else if(intent == "Default Fallback Intent"){
-     query = req.body.queryResult.queryText;
-	console.log("Calling FAQ URL for text - " + query);
-     callFAQ(query).then((output)=>{
-	     msg = output ? output.substr(0,15) : "Could not understand you";
-	     console.log("in then:");
-	     console.log(msg);
-	     return res.json(setResponse(res,msg,contextOut)); 
-	     
-     }).catch((error)=>{
-	     msg = "Could not get any answer";
-	     return res.json(setResponse(res,msg,contextOut)); 
-		       });     
-   }else{
-      if(contextsObject["phone_number"]){
-         phone_number = contextsObject["phone_number"]
-	 if(intent == "interviewRounds"){
-		 field = "INTERVIEW_ROUNDS";
-		 getCandidateField(query).then((output)=>{
-	     msg = output ? "There will be total of " + output + " rounds" : "Could not understand you";
-	     console.log(msg);
-		     return res.json(setResponse(res,msg,contextOut)); 	     
-	     }).catch((error)=>{
-		     msg = "Could not get any answer";
-		     return res.json(setResponse(res,msg,contextOut)); 
-			       });
-      }else{
-         msg = "bhaag yahaan se"; 
-	   res.json(setResponse(res,msg,contextOut)); 
-      }
-   }
-   
+            }
+        });
+        res.json(setResponse(res, msg, contextOut));
+    } else if (intent == "Default Fallback Intent") {
+        query = req.body.queryResult.queryText;
+        console.log("Calling FAQ URL for text - " + query);
+        callFAQ(query).then((output) => {
+            msg = output ? output.substr(0, 15) : "Could not understand you";
+            console.log("in then:");
+            console.log(msg);
+            return res.json(setResponse(res, msg, contextOut));
+
+        }).catch((error) => {
+            msg = "Could not get any answer";
+            return res.json(setResponse(res, msg, contextOut));
+        });
+    } else {
+        if (contextsObject["phone_number"]) {
+            phone_number = contextsObject["phone_number"]
+            if (intent == "interviewRounds") {
+                field = "INTERVIEW_ROUNDS";
+                getCandidateField(query).then((output) => {
+                    msg = output ? "There will be total of " + output + " rounds" : "Could not understand you";
+                    console.log(msg);
+                    return res.json(setResponse(res, msg, contextOut));
+                }).catch((error) => {
+                    msg = "Could not get any answer";
+                    return res.json(setResponse(res, msg, contextOut));
+                });
+            } else {
+                msg = "bhaag yahaan se";
+                res.json(setResponse(res, msg, contextOut));
+            }
+        }
+
+    }
 });
 
 app.get('/candidates', function (req, res) {
-   console.log("Got a GET request for the candidates");
-   client.query('SELECT * FROM "CANDIDATE";', (err, response) => {
-     if (err) {
-       console.log(err.stack);
-     } else {
-	   console.log(response.rows);
-       res.json(response.rows);
-     }
-   })
+    console.log("Got a GET request for the candidates");
+    client.query('SELECT * FROM "CANDIDATE";', (err, response) => {
+        if (err) {
+            console.log(err.stack);
+        } else {
+            console.log(response.rows);
+            res.json(response.rows);
+        }
+    })
 });
 
 app.get('/candidate/phone', function (req, res) {
-   console.log("Got a GET request for a candidate using phone number");
-   query_phone = req.query.phone;
-   client.query('SELECT * FROM "CANDIDATE" WHERE "PHONE_NUMBER" = ' + query_phone + ';', (err, response) => {
-     if (err) {
-       console.log(err.stack);
-     } else {
-	   console.log(response.rows);
-       res.json(response.rows);
-     }
-   })
+    console.log("Got a GET request for a candidate using phone number");
+    query_phone = req.query.phone;
+    client.query('SELECT * FROM "CANDIDATE" WHERE "PHONE_NUMBER" = ' + query_phone + ';', (err, response) => {
+        if (err) {
+            console.log(err.stack);
+        } else {
+            console.log(response.rows);
+            res.json(response.rows);
+        }
+    })
 });
 
-function setResponse(res,msg,contexts){
-   let responseObj={
-         "fulfillmentText":msg,
-         "fulfillmentMessages":[
-            {
-               "text": {
-               "text": [
-                  msg
-                  ]
-               }
+function setResponse(res, msg, contexts) {
+    let responseObj = {
+        "fulfillmentText": msg,
+        "fulfillmentMessages": [{
+            "text": {
+                "text": [
+                    msg
+                ]
             }
-         ],
-         "outputContexts": contexts
-      }
-   return responseObj;
+        }
+        ],
+        "outputContexts": contexts
+    }
+    return responseObj;
 }
 
-function callFAQ (query) {
-  return new Promise((resolve, reject) => {
-	  reqUrl = faq_url + encodeURI(query);
-	  console.log(reqUrl);
-    request({
-              url: reqUrl,
-              method: "GET"
-            }, function (error, response, body){
-              if (!error && response.statusCode == 200) {
+function callFAQ(query) {
+    return new Promise((resolve, reject) => {
+        reqUrl = faq_url + encodeURI(query);
+        console.log(reqUrl);
+        request({
+            url: reqUrl,
+            method: "GET"
+        }, function (error, response, body) {
+            if (!error && response.statusCode == 200) {
                 let response = body;
-		console.log("FAQ Response:");
-		console.log(response);
+                console.log("FAQ Response:");
+                console.log(response);
                 resolve(response);
-              }else{
-                console.error("FAQ error -> ",error);
+            } else {
+                console.error("FAQ error -> ", error);
                 reject(error)
-              }
             }
-          );
-  });
+        });
+    });
 }
 
-function getCandidateField(phoneNum, field){
-	reqUrl = rest_url + "/phone?phone=" + phoneNum;
-	
-  return new Promise((resolve, reject) => {
-	  reqUrl = faq_url + encodeURI(query);
-	  console.log(reqUrl);
-    request({
-              url: reqUrl,
-              method: "GET"
-            }, function (error, response, body){
-              if (!error && response.statusCode == 200) {
+function getCandidateField(phoneNum, field) {
+    reqUrl = rest_url + "/phone?phone=" + phoneNum;
+
+    return new Promise((resolve, reject) => {
+        reqUrl = faq_url + encodeURI(query);
+        console.log(reqUrl);
+        request({
+            url: reqUrl,
+            method: "GET"
+        }, function (error, response, body) {
+            if (!error && response.statusCode == 200) {
                 let response = JSON.stringify(body);
-		if(response.length == 0){
-		    resolve(record.field);
-		}else{
-			record = response[0];
-			console.log("REST Response:");
-			console.log(response);
-			resolve(record.field);
-		}
-              }else{
-                console.error("REST error -> ",error);
+                if (response.length == 0) {
+                    resolve(record.field);
+                } else {
+                    record = response[0];
+                    console.log("REST Response:");
+                    console.log(response);
+                    resolve(record.field);
+                }
+            } else {
+                console.error("REST error -> ", error);
                 reject(error)
-              }
             }
-          );
-  });	
+        });
+    });
 }
